ae404bd06a480b0450d0b5397ba68751d0fe6629
Vivek <mvivek3112@gmail.com>
2017-12-03 00:48:50 +0530

    initial 3/12

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+      $('.dropdown-trigger').dropdown();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+//$('.dropdown-trigger').dropdown();
+//var elem = document.querySelector('.dropdown-trigger');
+//var instance = new M.Dropdown(elem, options);
